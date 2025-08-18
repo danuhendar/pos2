8b3ed13cd863c4f08b710b330e415ef66d5566f2
danuhendar <danuhendar14@gmail.com>
2025-08-18 21:09:54 +0700

    fix: Update theme configuration to use production server addresses and increment app version

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,10 +7,10 @@ const themeConfig = {
     animation: 'animate__fadeInRight', // animate__fadeIn, animate__fadeInDown, animate__fadeInUp, animate__fadeInLeft, animate__fadeInRight, animate__slideInDown, animate__slideInLeft, animate__slideInRight, animate__zoomIn
     navbar: 'navbar-sticky', // navbar-sticky, navbar-floating, navbar-static
     semidark: false,
-    versi_app:'1.0.01-rc9',
-    host: '127.0.0.1',
+    versi_app:'1.0.01-rc10',
+    host: '103.161.184.203',
     hostrnd: '103.161.184.203',
-    host_ws: '127.0.0.1',
+    host_ws: '103.161.184.203',
     port_login:'5656',
     port_administrasi: '5656',
     port_listener: '7322',
