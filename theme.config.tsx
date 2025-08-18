5c827b77a3359b6b57c2e4441364b66765140722
danuhendar <danuhendar14@gmail.com>
2025-08-18 13:10:19 +0700

    fix: Update host configuration to use production server addresses

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,9 +8,9 @@ const themeConfig = {
     navbar: 'navbar-sticky', // navbar-sticky, navbar-floating, navbar-static
     semidark: false,
     versi_app:'1.0.01-rc9',
-    host: '127.0.0.1',
+    host: '103.161.184.203',
     hostrnd: '103.161.184.203',
-    host_ws: '127.0.0.1',
+    host_ws: '103.161.184.203',
     port_login:'5656',
     port_administrasi: '5656',
     port_listener: '7322',
