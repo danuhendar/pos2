302c9ea75136c29317b7308540245b749589ee08
danuhendar <57881523+danuhendar@users.noreply.github.com>
2025-08-25 09:56:32 +0700

    feat: Add Enter key functionality to WhatsApp input in FormSales component; update application version to 1.0.01-rc15

--- components/sales/FormSales.tsx
+++ components/sales/FormSales.tsx
@@ -318,6 +318,12 @@ const FormSales: React.FC<FormSalesProps> = ({ url, jenis, IDReport }) => {
     const SubmitPayment = () => {
         input4Ref.current.focus();
     }
+    const SavePosTransaksiFromWhatsApp = (e: { key: string; }) => {
+        if (e.key === 'Enter') {
+            // Move focus to input 2
+           InsPosTransaksiSales();
+        }
+    }
     const KeyItem = (e: { key: string; }) => {
         if (e.key === 'Enter') {
             // Move focus to input 2
@@ -1947,7 +1953,7 @@ const FormSales: React.FC<FormSalesProps> = ({ url, jenis, IDReport }) => {
                                                     </div>
                                                 </div>
                                                 <div className="col-span-2">
-                                                <InputTextTypeKeyDown   in_title={"No.WhatsApp (Ex. 6281216854443)"} in_classname_title={"mb-1"} in_classname_content={"w-full"} in_classname_sub_content={"form-input placeholder:text-white-dark disabled:bg-gray-200 rounded-3xl text-right text-xs"} data_options={undefined} isDisabled={false} event={FormInputNoWhatsApp} in_value={IN_NO_WHATSAPP} in_ref={undefined} in_event_keydown={InsPosTransaksiSales} />
+                                                <InputTextTypeKeyDown   in_title={"No.WhatsApp (Ex. 6281216854443)"} in_classname_title={"mb-1"} in_classname_content={"w-full"} in_classname_sub_content={"form-input placeholder:text-white-dark disabled:bg-gray-200 rounded-3xl text-right text-xs"} data_options={undefined} isDisabled={false} event={FormInputNoWhatsApp} in_value={IN_NO_WHATSAPP} in_ref={undefined} in_event_keydown={SavePosTransaksiFromWhatsApp} />
                                                 </div>
                                             </div>
                                             <div className="grid gap-3 lg:grid-cols-1 md:grid-cols-1">
